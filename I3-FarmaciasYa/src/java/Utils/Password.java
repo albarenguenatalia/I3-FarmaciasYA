4e07c8280cae6ddf0cf75133293aebaac2701a87
marhg <martingon4eo@gmail.com>
2015-10-03 20:44:03 -0300

    utils

--- I3-FarmaciasYa/src/java/Utils/Password.java
+++ I3-FarmaciasYa/src/java/Utils/Password.java
@@ -1,3 +1,5 @@
+package Utils;
+
  
  import java.security.MessageDigest;
  import java.security.NoSuchAlgorithmException;
--- I3-FarmaciasYa/src/java/Utils/prueba.java
+++ I3-FarmaciasYa/src/java/Utils/prueba.java
@@ -1,3 +1,5 @@
+package Utils;
+
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
