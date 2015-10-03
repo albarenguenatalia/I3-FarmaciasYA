9547d89c9b1ad09002f262a541b82a198243d9ce
marhg <martingon4eo@gmail.com>
2015-10-03 20:40:04 -0300

    mail

--- I3-FarmaciasYa/src/java/prueba.java
+++ I3-FarmaciasYa/src/java/prueba.java
@@ -3,13 +3,6 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  * @author martingonzalez
  */
@@ -20,5 +13,8 @@ public class prueba {
         System.out.println(Arrays.toString(p.getHash("estaesmypassord", "misalt".getBytes())).equals(Arrays.toString(p.getHash("estaesmypassord", "misalt".getBytes()))));
         System.out.println(Arrays.toString(p.getHash("estaesmy1passord", "misalt".getBytes())).equals(Arrays.toString(p.getHash("estaesmypassord", "misalt".getBytes()))));
 
+        Mail.sendMail("martingon4eo@gmail.com", "Mail de Prueba", "Hola! Esto es un mail de prueba del obligatorio de IS3");
+        String[] lista = {"martingon4eo@gmail.com","cynthia.barindelli@outlook.com","giqadel93@gmail.com","jona.amigo.ucu@gmail.com","albarenguenatalia@gmail.com"}; 
+        Mail.sendMail(lista, "Mail de Prueba", "Hola! Esto es un mail de prueba del obligatorio de IS3");
     }
 }
